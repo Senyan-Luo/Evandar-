68230d25ca8b0b2fa37f1cf1ed13c99ec41d08e0
Senyan L <sel152@ucsd.edu>
2018-11-08 09:40:19 -0800

    Homepage data passed from Firebase

--- js/main.js
+++ js/main.js
@@ -1,47 +1,24 @@
-const Events = [
-  {
-    title: "Python Workshop",
-    host: "UBIC",
-    date: "Nov. 15th, 2018",
-    time: "5:30pm to 6:30pm",
-    location: "CSE007",
-    tagline: "Come to learn Python with us!",
-    description:
-      "Hey y'all, this Friday we will have another python workshop, come to learn and hang out with us! Free Pizza and drinks will be provided.",
-    interest: "7"
-  },
-  {
-    title: "Machine Learning workshop",
-    host: "DS3",
-    date: "Nov. 20th, 2018",
-    time: "4:00pm to 5:00pm",
-    location: "SSC1307",
-    tagline: "The coolest intro crash course to machine learning.",
-    description:
-      "Hey guys! Welcome back to our biweekly data science workshop! This Thursday come hand out with Data Science Student Society and learn about machine learning!",
-    interest: "16"
-  }
-];
-
 $(document).ready(function() {
-  loadHomepage(Events);
+  loadHomepage();
+
   $(".navbar-toggler").on("click", function() {
     $(".animated-icon").toggleClass("open");
   });
-  $(".carousel").carousel();
+  $(".carousel").carousel({
+    interval: false
+  });
 
   $('[data-toggle="popover"]').popover({
     container: "#carouselDiv"
   });
+
   $(".popover-dismiss").popover({
     trigger: "focus"
   });
 
-  
 
-  function loadHomepage(data) {
+  function loadHomepage() {
     console.log("triggered loadHomepage");
-    
     $("#toggleBtn").on("click", function(event) {
       event.preventDefault();
       $(".main-body").toggleClass("listView");
@@ -82,31 +59,31 @@ $(document).ready(function() {
         </a>
       </div>
       `;
-      $(".main-body").html(carouselString);
-
-    data.forEach(item => {
-      let newEvent = document.createElement("div");
-      $(newEvent).addClass("carousel-item");
-      var domString = `
-                  <img class="d-block w-100" src="http://via.placeholder.com/800x300" alt="First slide">
-                  <div class="carousel-caption">
-                        <h3>${item.title}</h3>
-                        <p>Hosted by <a href="#">${item.host}</a></p>
-                        <h5>${item.date}</h5>
-                        <h5>${item.time}</h5>
-                        <h5>${item.location}</h5>
-                        <p>"${item.tagline}"</p>
-                      </div>
-                 
-<button tabindex="0" type="button" class="btn btn-secondary" style="margin-left:46%;" data-container="#carouselDiv" data-trigger="focus" data-toggle="popover" data-placement="top" data-content="${
-        item.description
-      }      ${item.interest} Likes">
-  Learn More
-</button>
-`;
-      $(newEvent).html(domString);
-      console.log(newEvent);
-      $("#carouselDiv").append(newEvent);
+    $(".main-body").html(carouselString);
+    eventsRef.get().then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        console.log("inside forEach loop");
+        console.log(doc.data());
+        let newEvent = document.createElement("div");
+        $(newEvent).addClass("carousel-item");
+        var domString = `
+                    <img class="d-block w-100" src="http://via.placeholder.com/800x300" alt="First slide">
+                    <div class="carousel-caption">
+                          <h3>${doc.data().title}</h3>
+                          <p>Hosted by <a href="#">${doc.data().host}</a></p>
+                          <h5>${doc.data().date}</h5>
+                          <h5>${doc.data().startTime}</h5>
+                          <h5>${doc.data().endTime}</h5>
+                          <h5>${doc.data().location}</h5>
+                          <p>"${doc.data().tagline}"</p>
+                         </div>
+                         <a tabindex="0" role="button" data-trigger="focus" data-placement="top" id="detailBtn" class="btn btn-secondary" style="margin-left:46%;" data-toggle="popover"  data-html="true" data-content="hello!" title="${doc.data().title}">
+                        Learn More
+                    </a>`;
+        $(newEvent).html(domString);
+        console.log(newEvent);
+        $("#carouselDiv").append(newEvent);
+      });
     });
   }
 });
