0a69b072215b7ff5aee1737cfecf6ee87c08a273
Senyan L <sel152@ucsd.edu>
2018-11-08 23:27:20 -0800

    Handlebars homepage redo

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
-  loadHomepage();
 
   $(".navbar-toggler").on("click", function() {
     $(".animated-icon").toggleClass("open");
   });
-  $(".carousel").carousel({
-    interval: false
-  });
+ 
+  $('.carousel').each(function(){
+    $(this).carousel({
+        interval: 10000,
+        pauseAutoPlayOnHover: true
+    });
+});
 
   $('[data-toggle="popover"]').popover({
-    container: "#carouselDiv"
+    container: ".body"
   });
 
   $(".popover-dismiss").popover({
     trigger: "focus"
   });
+  
 
+  var source = $("#carousel-template").html();
+  console.log(source)
+  var template = Handlebars.compile(source);
+  var carousel = $("#carouselDiv")
 
-  function loadHomepage() {
-    console.log("triggered loadHomepage");
+  eventsRef.get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      console.log("inside forEach loop");
+      console.log(doc.data().description);
+      var string = template(doc.data());
+      carousel.append(string);
+     
+    });
+  });
+  
+ 
+   
     $("#toggleBtn").on("click", function(event) {
       event.preventDefault();
       $(".main-body").toggleClass("listView");
@@ -31,59 +49,10 @@ $(document).ready(function() {
         $(".listView").html(listString);
       } else {
         $(".listView").empty();
-        $(".main-body").html(carouselString);
       }
     });
 
-    let carouselString = `
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner" id="carouselDiv">
-            
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src="http://via.placeholder.com/800x300" alt="First slide">
-                  <div class="carousel-caption">
-                        <h3>Welcome to Evendar!</h3>
-                        <h4>Click Right or Left to start discovering Student Orgs events on campus</h4>
-                      </div>
-            </div>
-            </div>
-            
-        
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-      `;
-    $(".main-body").html(carouselString);
-    eventsRef.get().then(snapshot => {
-      snapshot.docs.forEach(doc => {
-        console.log("inside forEach loop");
-        console.log(doc.data());
-        let newEvent = document.createElement("div");
-        $(newEvent).addClass("carousel-item");
-        var domString = `
-                    <img class="d-block w-100" src="http://via.placeholder.com/800x300" alt="First slide">
-                    <div class="carousel-caption">
-                          <h3>${doc.data().title}</h3>
-                          <p>Hosted by <a href="#">${doc.data().host}</a></p>
-                          <h5>${doc.data().date}</h5>
-                          <h5>${doc.data().startTime}</h5>
-                          <h5>${doc.data().endTime}</h5>
-                          <h5>${doc.data().location}</h5>
-                          <p>"${doc.data().tagline}"</p>
-                         </div>
-                         <a tabindex="0" role="button" data-trigger="focus" data-placement="top" id="detailBtn" class="btn btn-secondary" style="margin-left:46%;" data-toggle="popover"  data-html="true" data-content="hello!" title="${doc.data().title}">
-                        Learn More
-                    </a>`;
-        $(newEvent).html(domString);
-        console.log(newEvent);
-        $("#carouselDiv").append(newEvent);
-      });
-    });
-  }
+
+   
+  
 });
