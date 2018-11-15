9b4fd2066921da108933e8f3a1bcb9aaf583dc4a
aryakaul <a1kaul@ucsd.edu>
2018-11-14 21:51:46 -0800

    adding authentication logic

--- js/main.js
+++ js/main.js
@@ -72,11 +72,33 @@ $('.carousel').carousel({
 })
   
  
+//ARYA'S STUFF PROBABLY ALL WRONG
 
+var actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be whitelisted in the Firebase Console.
+  url: 'https://senyan-luo.github.io/Evendar/pages/add_event.html',
+  // This must be true.
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: 'com.example.ios'
+  },
+  android: {
+    packageName: 'com.example.android',
+    installApp: true,
+    minimumVersion: '12'
+  }
+}
+
+firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+  .then(function() {
+    // The link was successfully sent. Inform the user.
+    // Save the email locally so you don't need to ask the user for it again
+    // if they open the link on the same device.
+    window.localStorage.setItem('emailForSignIn', email);
+  })
+  .catch(function(error) {
+    // Some error occurred, you can inspect the code: error.code
+  });;
     
-
-  
-
-   
-  
 
